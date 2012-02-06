3b8f89c1032ed5fcee634804891baab130a28393
Mathurin BODY <mathurin.body@gmail.com>
2012-02-06 16:15:56 +0100

    QuestionTest

---
+++ test/QuestionsTest.java
@@ -0,0 +1,21 @@
+import org.junit.Test;
+import play.mvc.Http.Response;
+import play.test.FunctionalTest;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Mathurin
+ * Date: 06/02/12
+ * Time: 16:08
+ * To change this template use File | Settings | File Templates.
+ */
+public class QuestionsTest extends FunctionalTest {
+
+    @Test
+    public void getQuestionsTest() {
+        Response addQuestionResponse = POST("/question/", APPLICATION_X_WWW_FORM_URLENCODED, "question.difficulte=1");
+        assertStatus(302, addQuestionResponse);
+        Response listQuestionsResponse = GET("/questions");
+        assertIsOk(listQuestionsResponse);
+    }
+}
