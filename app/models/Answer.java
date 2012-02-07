379930eedcdfe62a7f412349fde88fb15a4a5ba2
unknown <AREVEL@TOURSTA25.(none)>
2012-02-07 11:13:05 +0100

    Création des entités

---
+++ app/models/Answer.java
@@ -0,0 +1,25 @@
+package models;
+
+import play.db.jpa.Model;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: AREVEL
+ * Date: 07/02/12
+ * Time: 10:59
+ * To change this template use File | Settings | File Templates.
+ */
+@Entity
+public class Answer extends Model {
+    
+    public int mark;
+    public String comment;
+    @ManyToOne
+    public Question question;
+    @ManyToOne
+    public Interview interview;
+
+}
---
+++ app/models/Interview.java
@@ -0,0 +1,24 @@
+package models;
+
+import play.db.jpa.Model;
+
+import javax.persistence.Entity;
+import java.util.Calendar;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: AREVEL
+ * Date: 07/02/12
+ * Time: 10:48
+ * To change this template use File | Settings | File Templates.
+ */
+@Entity
+public class Interview extends Model {
+
+    public String candidateName;
+    public String candidateFirstName;
+    public Calendar interviewDate;
+    public String assessorName;
+    public String assessorFirstName;
+    
+}
