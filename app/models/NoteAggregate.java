9bd86bd2ac05ce5b80ef8982fd5df663a33ebcfa
Christophe Jollivet <christophe.jollivet@gmail.com>
2012-02-10 14:51:04 +0100

    added data for dev

---
+++ app/models/NoteAggregate.java
@@ -0,0 +1,11 @@
+package models;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: christophe
+ * Date: 10/02/12
+ * Time: 12:22
+ * To change this template use File | Settings | File Templates.
+ */
+public class NoteAggregate {
+}
